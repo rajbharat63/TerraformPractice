160d73d7d311f9fe43bbdf6886cf39513cfc1ed1
Raj <bharatrajn@gmail.com>
2026-03-28 16:06:51 -0400

    eee

--- Day-6-Terraform-statefile-lock/main.tf
+++ Day-6-Terraform-statefile-lock/main.tf
@@ -14,10 +14,26 @@ resource "aws_instance" "UAT" {
 }
 
 resource "aws_subnet" "public2_subnet" {
-    vpc_id = aws_vpc.vpc_new.id
+    vpc_id = aws_vpc.vpc_old.id
     cidr_block = "10.0.0.0/24"
     tags = {
       "Name" = "public2 subnet"
     }
   
+}
+
+resource "aws_subnet" "public3_subnet" {
+    vpc_id = aws_vpc.vpc_old.id
+    cidr_block = "10.0.1.0/25"
+    tags = {
+      "Name" = "public3 subnet"
+    }
+  
+}
+resource "aws_instance" "UAT123" {
+    ami =  "ami-02dfbd4ff395f2a1b"
+    instance_type = "t2.medium"
+    tags = {
+      Name = "UAT123-instance"
+    }
 }

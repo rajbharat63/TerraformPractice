2f11d765ef468565e23f136a9f80f752d335241e
Raj <bharatrajn@gmail.com>
2026-03-28 15:50:15 -0400

    new-day6

--- Day-6-Terraform-statefile-lock/backend.tf
+++ Day-6-Terraform-statefile-lock/backend.tf
@@ -1,7 +1,8 @@
 terraform {
   backend "s3" {
     bucket = "raj20567"
-    key    = "Day1/terraform.tfstate"
+    key    = "Day2/terraform.tfstate"
     region = "us-east-1"
+    use_lockfile = true #Dynamo no longer required for state locking in s3 backend we can use lockfile
   }
 }
--- Day-6-Terraform-statefile-lock/main.tf
+++ Day-6-Terraform-statefile-lock/main.tf
@@ -1,23 +1,23 @@
-resource "aws_vpc" "vpc_new" {
+resource "aws_vpc" "vpc_old" {
     cidr_block = "10.0.0.0/16"
     tags = {
-      "Name" = "production"
+      "Name" = "old_UAT"
     }
 }
 
-resource "aws_instance" "test" {
+resource "aws_instance" "UAT" {
     ami =  "ami-02dfbd4ff395f2a1b"
     instance_type = "t2.micro"
     tags = {
-      Name = "test-instance"
+      Name = "UAT-instance"
     }
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public2_subnet" {
     vpc_id = aws_vpc.vpc_new.id
     cidr_block = "10.0.0.0/24"
     tags = {
-      "Name" = "public subnet"
+      "Name" = "public2 subnet"
     }
   
 }
